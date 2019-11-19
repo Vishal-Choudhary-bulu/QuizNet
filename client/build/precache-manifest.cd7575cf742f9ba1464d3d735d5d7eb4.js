4b9a17a46156d61b39246de27d5df057a9ea4316
Vishal-Choudhary-Bulu <officialbulu@gmail.com>
2019-11-19 09:59:33 +0530

    Production build made

---
+++ client/build/precache-manifest.cd7575cf742f9ba1464d3d735d5d7eb4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dd31e33a67725422885769c96d92582",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b0d591f2171c4e40d2b",
+    "url": "/static/css/main.64ff963d.chunk.css"
+  },
+  {
+    "revision": "f441449f8f43a03c6e36",
+    "url": "/static/js/2.094e2121.chunk.js"
+  },
+  {
+    "revision": "7b0d591f2171c4e40d2b",
+    "url": "/static/js/main.5dcfbd6b.chunk.js"
+  },
+  {
+    "revision": "875d570ce1b6fc43afc6",
+    "url": "/static/js/runtime-main.e077b804.js"
+  }
+]);
